b83ed686ec1d84df7a19750c958b165147f3c855
Ryugi Suguro <rsuguro@gmail.com>
2021-04-19 21:08:12 +0900

    Header - Logo image made into a link

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import { Link } from "react-router-dom";
 
 function Header() {
   return <nav className="header">
 
   {/* logo on the left -> img */}
-  <img
-    className="header_logo"
-    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-    alt=""
-    />
+  <Link to="/">
+    <img
+      className="header_logo"
+      src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      alt=""
+      />
+  </Link>
 
   {/* Search box */}
 
