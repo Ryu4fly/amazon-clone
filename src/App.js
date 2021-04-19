897804148efe57c0642fed7377913094301fe5e5
Ryugi Suguro <rsuguro@gmail.com>
2021-04-19 23:49:09 +0900

    Banner image + gradient added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -17,7 +19,7 @@ function App() {
           {/* This is the default route */}
           <Route path="/">
             <Header />
-            <h1>HOME PAGE!!!</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
