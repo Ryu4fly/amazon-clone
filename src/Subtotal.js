4e81fdc3633cb87c502e71375121acb695d766f2
Ryugi Suguro <rsuguro@gmail.com>
2021-04-28 13:28:52 +0900

    Subtotal added

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,34 @@
+import { SportsBasketball } from '@material-ui/icons';
 import React from 'react';
+import CurrencyFormat from 'react-currency-format';
 import './Subtotal.css';
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       {/* Price */}
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Part of the homework */}
+              Subtotal ({basket.length} items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"￥"}
+      />
       <button>Proceed to Checkout</button>
     </div>
   )
